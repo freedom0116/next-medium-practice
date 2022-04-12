0c04e69e257792f52c845c1ff22dfc79f8b6e800
DESKTOP-Q61474S\Jimmy <jimmy29257415@gmail.com>
2022-04-12 11:05:36 +0800

    feat: adjust the structure in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,53 @@
 import Link from 'next/link';
 
+const HeaderArea = ({ children }: any) => (
+  <header className="flex justify-between p-5">{children}</header>
+);
+
+const HeaderLeft = ({ children }: any) => (
+  <div className="flex items-center space-x-5">{children}</div>
+);
+
+const HeaderRight = ({ children }: any) => (
+  <div className="flex items-center space-x-5 text-green-600">{children}</div>
+);
+
+const Logo = ({ src }: any) => (
+  <img className="w-44 object-contain cursor-pointer" src={src} alt="" />
+);
+
+const Nav = ({ children }: any) => (
+  <div className="hidden md:inline-flex space-x-5">{children}</div>
+);
+
+const NavItemGreen = ({ children }: any) => (
+  <div className="text-white bg-green-600 px-4 py-1 rounded-full">
+    {children}
+  </div>
+);
+
 function Header() {
   return (
-    <header className="flex justify-between p-5">
-      <div className="flex items-center space-x-5">
+    <HeaderArea>
+      <HeaderLeft>
         <Link href="/" passHref>
-          <img
-            className="w-44 object-contain cursor-pointer"
-            src="https://deanlife.blog/wp-content/uploads/2021/02/medium-1024x254.png"
-            alt=""
-          />
+          <Logo src="https://deanlife.blog/wp-content/uploads/2021/02/medium-1024x254.png" />
         </Link>
 
-        <div className="hidden md:inline-flex space-x-5">
+        <Nav>
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
-            Follow
-          </h3>
-        </div>
-      </div>
+          <NavItemGreen>Follow</NavItemGreen>
+        </Nav>
+      </HeaderLeft>
 
-      <div className="flex items-center space-x-5 text-green-600">
+      <HeaderRight>
         <h3>Sign In</h3>
         <h3 className="border px-4 rounded-full border-green-600">
           Get Started
         </h3>
-      </div>
-    </header>
+      </HeaderRight>
+    </HeaderArea>
   );
 }
 
